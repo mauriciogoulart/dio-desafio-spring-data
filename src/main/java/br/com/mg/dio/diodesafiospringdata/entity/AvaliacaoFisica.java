826dd0eafcbf6b09b62a1fb1238e148eaed37533
mauriciogoulart <mauryciorgoulart@yahoo.com.br>
2022-05-06 21:14:28 -0300

    changed models configurations

--- src/main/java/br/com/mg/dio/diodesafiospringdata/entity/AvaliacaoFisica.java
+++ src/main/java/br/com/mg/dio/diodesafiospringdata/entity/AvaliacaoFisica.java
@@ -3,18 +3,25 @@ package br.com.mg.dio.diodesafiospringdata.entity;
 import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_avaliacoes_fisicas")
 public class AvaliacaoFisica {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	@ManyToOne
 	private Aluno aluno;
